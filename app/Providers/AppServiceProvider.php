5a28eae448bd7dd158da17b5607dba015b8a1354
Amer Nafis <amernafis1103@gmail.com>
2025-10-27 23:00:59 +0800

    fix: Force HTTPS 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('APP_ENV') === 'production') {
+        if (config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
     }
